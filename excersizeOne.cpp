dd0d1d29c2bf63b10c21eb8c2b56f1e717a0e7f8
alishaf18 <alishafaraz@gmail.com>
2025-04-14 22:20:50 -0700

    first assignment!

---
+++ excersizeOne.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// excersize one
+/*int main() {
+    cout << "\t\tIf it's ain't broke don't fix it!" << endl;
+    cout << "123" << endl;
+    cout << "456\n";
+    cout << "789" << endl;
+    cout << "123" <<endl << "456" << endl << "789" <<endl;
+    return 0;
+}*/
+
+//excersize two
+int  main () {
+       cout << "*\n";
+       cout << "**\n";
+       cout << "***\n";
+       cout << "****\n";
+       cout << "*****\n";
+
+       return 0;
+}
+
+/*g++ excersizeOne.cpp -o excersizeOne
+./excersizeOne*/
